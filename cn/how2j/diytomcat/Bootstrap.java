bb80a02aae00c480fed15cb8407353b513e616c2
wei <iswei.wei0307@gmail.com>
2022-08-22 21:58:27 -0400

    Initial commit

---
+++ cn/how2j/diytomcat/Bootstrap.java
@@ -0,0 +1,83 @@
+package cn.how2j.diytomcat;
+
+import cn.how2j.diytomcat.http.Request;
+import cn.how2j.diytomcat.http.Response;
+import cn.how2j.diytomcat.util.Constant;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.NetUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.ZipUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+
+        try {
+            int port = 18080;
+
+            if(!NetUtil.isUsableLocalPort(port)) {
+                System.out.println(port +" 端口已经被占用了，排查并关闭本端口的办法请用：\r\nhttps://how2j.cn/k/tomcat/tomcat-portfix/545.html");
+                return;
+            }
+            ServerSocket ss = new ServerSocket(port);
+
+            while(true) {
+                Socket s =  ss.accept();
+                Request request = new Request(s);
+                System.out.println("浏览器的输入信息： \r\n" + request.getRequestString());
+                System.out.println("uri:" + request.getUri());
+
+                Response response = new Response();
+
+                String uri = request.getUri();
+                if(null==uri)
+                    continue;
+                System.out.println(uri);
+                if("/".equals(uri)){
+                    String html = "Hello DIY Tomcat from how2j.cn";
+                    response.getWriter().println(html);
+                }
+                else{
+                    String fileName = StrUtil.removePrefix(uri, "/");
+                    File file = FileUtil.file(Constant.rootFolder,fileName);
+                    if(file.exists()){
+                        String fileContent = FileUtil.readUtf8String(file);
+                        response.getWriter().println(fileContent);
+                    }
+                    else{
+                        response.getWriter().println("File Not Found");
+                    }
+                }
+                handle200(s, response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void handle200(Socket s, Response response) throws IOException {
+        String contentType = response.getContentType();
+        String headText = Constant.response_head_202;
+        headText = StrUtil.format(headText, contentType);
+        byte[] head = headText.getBytes();
+
+        byte[] body = response.getBody();
+
+        byte[] responseBytes = new byte[head.length + body.length];
+        ArrayUtil.copy(head, 0, responseBytes, 0, head.length);
+        ArrayUtil.copy(body, 0, responseBytes, head.length, body.length);
+
+        OutputStream os = s.getOutputStream();
+        os.write(responseBytes);
+        s.close();
+    }
+}
